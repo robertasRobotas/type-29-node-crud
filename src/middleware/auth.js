fc6fb7b766118be2f4ef5eedb19168fa134f3b78
Robertas Ankudovicius <r.ankudovicius@gmail.com>
2026-01-23 08:35:20 +0200

    Auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const authUser = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Bad auth (no token)" });
+  }
+
+  jwt.verify(token, process.env.JWT_RANDOMISER, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Bad auth (no token)" });
+    }
+
+    // req.body = req.body || {};
+    // req.body.userId = decoded.userId;
+
+    next();
+  });
+};
+
+export default authUser;
--- src/router/car.js
+++ src/router/car.js
@@ -6,17 +6,18 @@ import {
   updateCarById,
   deleteCarById,
 } from "../controller/car.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/cars", getAllCars);
+router.get("/cars", auth, getAllCars);
 
-router.get("/cars/:id", getCarById);
+router.get("/cars/:id", auth, getCarById);
 
-router.post("/cars", insertCar);
+router.post("/cars", auth, insertCar);
 
-router.put("/cars/:id", updateCarById);
+router.put("/cars/:id", auth, updateCarById);
 
-router.delete("/cars/:id", deleteCarById);
+router.delete("/cars/:id", auth, deleteCarById);
 
 export default router;
